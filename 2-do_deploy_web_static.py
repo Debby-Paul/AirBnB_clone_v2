3b76d7ed481a0356682e49f55eccf855d2dcc0f7
Emem Udofia <ememudofia456@gmail.com>
2022-12-22 14:02:34 -0800

    2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,49 +1,40 @@
 #!/usr/bin/python3
-"""
-script that distributes archive to webservers
-"""
-import os.path
+""" Function that compress a folder """
+from datetime import datetime
 from fabric.api import *
-from fabric.operations import run, put, sudo
+import shlex
+import os
 
-env.hosts = ['100.25.19.204', '54.157.159.85']
-env.user = 'ubuntu'
-env.key_filename = '~/.ssh/alx_server'
+
+env.hosts = ['44.211.97.124', '34.228.52.136']
+env.user = "ubuntu"
+
 
 def do_deploy(archive_path):
-    """ This function distributes an archive to your web servers"""
-    if (os.path.isfile(archive_path) is False):
+    """ Deploys """
+    if not os.path.exists(archive_path):
         return False
-
     try:
-        new_comp = archive_path.split("/")[-1]
-        new_folder = ("/data/web_static/releases/" + new_comp.split(".")[0])
+        name = archive_path.replace('/', ' ')
+        name = shlex.split(name)
+        name = name[-1]
 
-        # Upload the archive to the /tmp/ directory of the web server
+        wname = name.replace('.', ' ')
+        wname = shlex.split(wname)
+        wname = wname[0]
+
+        releases_path = "/data/web_static/releases/{}/".format(wname)
+        tmp_path = "/tmp/{}".format(name)
+
         put(archive_path, "/tmp/")
-
-        # Uncompress the archive to the folder /data/web_static/releases/<archive filename
-        # without extension> on the web server
-        run("sudo mkdir -p {}".format(new_folder))
-        run("sudo tar -xzf /tmp/{} -C {}".
-            format(new_comp, new_folder))
-
-        # Delete the archive from the web server
-        run("sudo rm /tmp/{}".format(new_comp))
-
-		# move contents into host web_static
-        run("sudo mv {}/web_static/* {}/".format(new_folder, new_folder))
-        
-        # remove extraneous web_static dir
-        run("sudo rm -rf {}/web_static".format(new_folder))
-
-        # Delete the symbolic link /data/web_static/current from the web server
-        run('sudo rm -rf /data/web_static/current')
-
-        # Create a new the symbolic link /data/web_static/current on the web server,
-        # linked to the new version of your code (/data/web_static/releases/<archive filename without extension>)
-        run("sudo ln -s {} /data/web_static/current".format(new_folder))
-
+        run("mkdir -p {}".format(releases_path))
+        run("tar -xzf {} -C {}".format(tmp_path, releases_path))
+        run("rm {}".format(tmp_path))
+        run("mv {}web_static/* {}".format(releases_path, releases_path))
+        run("rm -rf {}web_static".format(releases_path))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {} /data/web_static/current".format(releases_path))
+        print("New version deployed!")
         return True
     except:
         return False
